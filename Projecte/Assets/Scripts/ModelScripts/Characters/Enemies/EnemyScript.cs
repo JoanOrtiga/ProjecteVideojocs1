8bd52bc1f59a1f301f60cdd50aafaa1256d98010
JuliaBlasco <47471443+JuliaBlasco@users.noreply.github.com>
2019-05-22 00:47:16 +0200

    heyboyyy

--- Projecte/Assets/Scripts/ModelScripts/Characters/Enemies/EnemyScript.cs
+++ Projecte/Assets/Scripts/ModelScripts/Characters/Enemies/EnemyScript.cs
@@ -28,53 +28,27 @@ public class EnemyScript : MonoBehaviour
     void Start ()
     {
         if (_type == EnemyType.Trunk)
-        {
-           // _health = GameManager.instance
-        }
+            _health = GameManager.instance.trunkModel._health;
         else if (_type == EnemyType.Spider)
-        {
-            
-        }
-        else if (_type == EnemyType.Spider)
-        {
-
-        }
+            _health = GameManager.instance.spiderModel._health;
         else if (_type == EnemyType.Bat)
-        {
-
-        }
+            _health = GameManager.instance.batModel._health;
         else if (_type == EnemyType.Skeleton)
-        {
-
-        }
+            _health = GameManager.instance.skeletonModel._health;
         else if (_type == EnemyType.Swamp)
-        {
-
-        }
+            _health = GameManager.instance.swampModel._health;
         else if (_type == EnemyType.Vampire)
-        {
-
-        }
+            _health = GameManager.instance.vampireModel._health;
         else if (_type == EnemyType.Devil1)
-        {
-
-        }
+            _health = GameManager.instance.devil1Model._health;
         else if (_type == EnemyType.Devil2)
-        {
-
-        }
+            _health = GameManager.instance.devil2Model._health;
         else if (_type == EnemyType.Devil3)
-        {
-
-        }
+            _health = GameManager.instance.devil3Model._health;
         else if (_type == EnemyType.Warrior)
-        {
-
-        }
+            _health = GameManager.instance.warriorModel._health;
         else if (_type == EnemyType.Magician)
-        {
-
-        }
+            _health = GameManager.instance.magicianModel._health;
     }
 	
 	// Update is called once per frame
