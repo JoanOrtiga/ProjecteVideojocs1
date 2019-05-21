5e53428558a2954adfe57c4ecd18ea1d768dfb66
JoanStark <47462893+JoanStark@users.noreply.github.com>
2019-05-21 20:52:53 +0200

    Master00

---
+++ Projecte/Assets/Scripts/Camera/ZoomInOut/CameraZoomInOut.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomInOut : MonoBehaviour {
+
+    public bool zoomX;
+    public bool zoomY;
+
+    public bool leftToRight_Or_DownToUp = true;
+
+    public float zoomOut = 10f;
+    public float interpolationRange = 7f;
+
+    private float normalZoom =  10f;
+
+    float size;
+
+    private void Start()
+    {
+        normalZoom = Camera.main.orthographicSize;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (zoomY)
+            size = GetComponent<Collider2D>().bounds.size.y;
+        else if (zoomX)
+            size = GetComponent<Collider2D>().bounds.size.x;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        if (zoomY)
+        {
+            if (Input.GetAxis("Vertical") < 0 && leftToRight_Or_DownToUp || Input.GetAxis("Vertical") > 0 && !leftToRight_Or_DownToUp)
+                Camera.main.orthographicSize = normalZoom;
+            else if (Input.GetAxis("Vertical") > 0 && leftToRight_Or_DownToUp || Input.GetAxis("Vertical") < 0 && !leftToRight_Or_DownToUp)
+                Camera.main.orthographicSize = zoomOut;
+        }
+        else if (zoomX)
+        {
+            if (Input.GetAxis("Horizontal") < 0 && leftToRight_Or_DownToUp || Input.GetAxis("Horizontal") > 0 && !leftToRight_Or_DownToUp)
+                Camera.main.orthographicSize = normalZoom;
+            else if (Input.GetAxis("Horizontal") > 0 && leftToRight_Or_DownToUp || Input.GetAxis("Horizontal") < 0 && !leftToRight_Or_DownToUp)
+                Camera.main.orthographicSize = zoomOut;    
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (zoomY)
+            YZoom(collision);
+        else if (zoomX)
+            XZoom(collision);
+    }
+
+
+
+    /*
+     * 
+     * ESTO ESTA WORK IN PROGRES. NUESTRA INTENCIÓN ES QUE SEGUN LA POSICIÓN DEL PLAYER DENTRO DEL TRIGGER, LA CAMARA TENGA UN ZOOM O OTRO.
+     * DEMOMENTO LO HEMOS HECHO FUNCIONAR A MEDIAS.
+     * 
+     */
+
+
+    private void YZoom(Collider2D collision)
+    {
+        float interpolation_value;
+
+        if (Input.GetAxis("Vertical") > 0 && leftToRight_Or_DownToUp || Input.GetAxis("Vertical") < 0 && !leftToRight_Or_DownToUp)
+        {
+            //interpolation_value = Mathf.Max(0, ((collision.transform.position.y - transform.position.y + size) / (transform.position.y - transform.position.y + size)) * (interpolationRange - 0) + 0);
+
+            interpolation_value = (Input.GetAxis("Vertical")) * 5f;
+
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, zoomOut, Time.deltaTime * interpolation_value);
+        }
+        else if (Input.GetAxis("Vertical") < 0 && leftToRight_Or_DownToUp || Input.GetAxis("Vertical") > 0 && !leftToRight_Or_DownToUp) 
+        {
+            //interpolation_value = (((collision.transform.position.y - transform.position.y) / (transform.position.y + size - transform.position.y)) * (interpolationRange - 0) + 0) * -1;
+
+            interpolation_value = (Input.GetAxis("Vertical") * -1) * 5f;
+
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, normalZoom, Time.deltaTime * interpolation_value);
+        }
+    }
+
+    private void XZoom(Collider2D collision)
+    {
+        float interpolation_value;
+
+        if (Input.GetAxis("Horizontal") < 0 && leftToRight_Or_DownToUp || Input.GetAxis("Horizontal") < 0 && !leftToRight_Or_DownToUp)
+        {
+            //  interpolation_value = ((collision.transform.position.x - transform.position.x + size) / (transform.position.x - transform.position.x + size)) * (interpolationRange - 0) + 0;
+
+            interpolation_value = (Input.GetAxis("Horizontal") * -1)  * 5f;
+
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, zoomOut, Time.deltaTime * interpolation_value);
+        }
+        else if (Input.GetAxis("Horizontal") > 0 && leftToRight_Or_DownToUp || Input.GetAxis("Horizontal") > 0 && !leftToRight_Or_DownToUp)
+        {
+            //   interpolation_value = (((collision.transform.position.x - transform.position.x) / (transform.position.x + size - transform.position.x)) * (interpolationRange - 0) + 0) * -1;
+
+            interpolation_value = (Input.GetAxis("Horizontal")) * 5f;
+
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, normalZoom, Time.deltaTime * interpolation_value);
+        }
+    }
+}
+
---
+++ Projecte/Assets/Scripts/EnemyIA/Boss1/Attack1State_Boss1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Boss1State))]
+[RequireComponent(typeof(MovingState_Boss1))]
+public class Attack1State_Boss1 : Boss1State {
+
+    public GameObject spikes;
+    private float time1 = 0.1f;
+    private float time2 = 3f;
+
+    public float radius;
+    public Vector3 center;
+
+
+    private void OnEnable()
+    {
+        StartCoroutine(attack1());
+    }
+
+    IEnumerator attack1()
+    {
+        yield return new WaitForSeconds(time1);
+        Instantiate(spikes, new Vector2(Random.Range(center.x - radius / 2, center.x + radius / 2), Random.Range(center.y - radius / 2, center.y + radius / 2)), transform.rotation);
+        yield return new WaitForSeconds(time1);
+        Instantiate(spikes, new Vector2(Random.Range(center.x - radius / 2, center.x + radius / 2), Random.Range(center.y - radius / 2, center.y + radius / 2)), transform.rotation);
+        yield return new WaitForSeconds(time1);
+        Instantiate(spikes, new Vector2(Random.Range(center.x - radius / 2, center.x + radius / 2), Random.Range(center.y - radius / 2, center.y + radius / 2)), transform.rotation);
+        yield return new WaitForSeconds(time1);
+        Instantiate(spikes, new Vector2(Random.Range(center.x - radius / 2, center.x + radius / 2), Random.Range(center.y - radius / 2, center.y + radius / 2)), transform.rotation);
+        yield return new WaitForSeconds(time1);
+        Instantiate(spikes, new Vector2(Random.Range(center.x - radius / 2, center.x + radius / 2), Random.Range(center.y - radius / 2, center.y + radius / 2)), transform.rotation);
+        yield return new WaitForSeconds(time2);
+
+        GetComponent<MovingState_Boss1>().attackingFalse();
+        GetComponent<Attack1State_Boss1>().enabled = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(100, 100, 100);
+        Gizmos.DrawSphere(center, radius);
+    }
+}
