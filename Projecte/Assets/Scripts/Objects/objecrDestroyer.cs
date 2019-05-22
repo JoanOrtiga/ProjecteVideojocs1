d4306e271aa19a034a470e187746efec380aa159
LadyMarina <sharkk244@gmail.com>
2019-05-22 15:41:25 +0200

    tumb destroyer

---
+++ Projecte/Assets/Scripts/Objects/objecrDestroyer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objecrDestroyer : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Smash()
+    {
+       anim.SetBool("smash", true);
+     //   StartCoroutine(breakCo());
+        
+    }
+    IEnumerable breakCo()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+       
+    
+}
