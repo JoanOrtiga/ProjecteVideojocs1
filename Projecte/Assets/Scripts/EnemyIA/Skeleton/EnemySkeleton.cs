44688f4e1f95d54d9f5897d33d9aa5a9a8bbcb65
LadyMarina <sharkk244@gmail.com>
2019-05-25 15:05:45 +0200

    SKELETON ENEMY

--- Projecte/Assets/Scripts/EnemyIA/Skeleton/EnemySkeleton.cs
+++ Projecte/Assets/Scripts/EnemyIA/Skeleton/EnemySkeleton.cs
@@ -10,15 +10,19 @@ public class EnemySkeleton : MonoBehaviour
     public float moveSpeed;
 
 
+    private Rigidbody2D myRigidbody;
     public Transform target;
     public float chaseRadius;
     public float attackRedius;
     public Transform homePosition;
+    public Animator anim;
 
 	// Use this for initialization
 	void Start ()
     {
+        anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
+        myRigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -31,7 +35,63 @@ public class EnemySkeleton : MonoBehaviour
     {
         if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRedius )
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            myRigidbody.MovePosition(temp);
+            changeAnim(temp - transform.position);
+           
+            Debug.Log("PERSEGUINT");
+            anim.SetBool("walking", true);
         }
+        if (Vector3.Distance(target.position, transform.position) < attackRedius)
+        {
+            Debug.Log("ATAKAN");
+            anim.SetBool("attaking", true);
+        }
+        else if (!(Vector3.Distance(target.position, transform.position) < attackRedius))
+        {
+            anim.SetBool("attaking", false);
+        }
+        if (Vector3.Distance(target.position, transform.position) > chaseRadius)
+        {
+            anim.SetBool("walking", false);
+        }
+        
     }
+
+
+    private void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+
+    private void changeAnim(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if (direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if (direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+
+        }
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if (direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if (direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
+            }
+        }
+        
+    }
+
+
 }
