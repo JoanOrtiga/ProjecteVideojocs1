f8ba9f977cc5f63a14cda575157e4ba97849a28a
LadyMarina <sharkk244@gmail.com>
2019-05-25 13:28:05 +0200

    skeletn enemy

---
+++ Projecte/Assets/Scripts/EnemyIA/Skeleton/EnemySkeleton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkeleton : MonoBehaviour
+{
+    public int health;
+    public string enemyName;
+    public int baseAttack;
+    public float moveSpeed;
+
+
+    public Transform target;
+    public float chaseRadius;
+    public float attackRedius;
+    public Transform homePosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ChechDistance();
+	}
+
+    void ChechDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRedius )
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
