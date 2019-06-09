8d75cf60d1e0abc327dd47fd01659f27d8f3cfc3
JuliaBlasco <47471443+JuliaBlasco@users.noreply.github.com>
2019-06-09 13:18:46 +0200

    fsdrtg

---
+++ Projecte/Assets/Pause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour {
+
+    GameObject pausePanel;
+    bool active; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        pausePanel = GameObject.FindGameObjectWithTag("Pause");
+        pausePanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetButtonDown("Pause"))
+        {
+            print("GAS");  
+            active = !active;
+            pausePanel.SetActive(active);
+
+            Time.timeScale = (active) ? 0 : 1f;
+        }	
+	}
+}
