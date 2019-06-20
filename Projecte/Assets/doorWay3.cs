c6bff20a57be504545cc53da37bb55df76cc8e4d
JuliaBlasco <47471443+JuliaBlasco@users.noreply.github.com>
2019-06-21 00:10:34 +0200

    krshgikuhs

---
+++ Projecte/Assets/doorWay3.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorWay3 : MonoBehaviour {
+
+    private Vector2 playerPosition;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (collision.GetComponent<PlayPickupRelicInvetory>().relic_bible && collision.GetComponent<PlayPickupRelicInvetory>().relic_grail)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
