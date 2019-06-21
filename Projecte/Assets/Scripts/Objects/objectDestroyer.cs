55df4554b17ef868d4e81399d5ede3cdf8d5c09d
LadyMarina <sharkk244@gmail.com>
2019-06-21 14:57:21 +0200

    Revert "jvhbkj"
    This reverts commit 3297de2df2925595053e08f3f7d8d2c30eeaf4c4.

--- Projecte/Assets/Scripts/Objects/objectDestroyer.cs
+++ Projecte/Assets/Scripts/Objects/objectDestroyer.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class objectDestroyer : MonoBehaviour
 {
-    public AudioClip audioDestroy;
-    public AudioSource destroyAudioSource;
     private Animator anim;
 
     // Use this for initialization
     void Start()
     {
-        destroyAudioSource.clip = audioDestroy;
         anim = GetComponentInParent<Animator>();
     }
 
@@ -19,7 +16,6 @@ public class objectDestroyer : MonoBehaviour
     public void Smash()
     {
         anim.SetBool("smash", true);
-        destroyAudioSource.Play();
         Destroy(gameObject.transform.parent.gameObject, .3f);
         //Destroy(gameObject, .3f);
     }
