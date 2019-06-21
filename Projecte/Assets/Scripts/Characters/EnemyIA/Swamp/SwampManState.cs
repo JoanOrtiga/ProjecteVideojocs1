75878c9e9298737dcdb8c0d90f2d774d6cef1771
LadyMarina <sharkk244@gmail.com>
2019-06-21 15:12:59 +0200

    joan master

---
+++ Projecte/Assets/Scripts/Characters/EnemyIA/Swamp/SwampManState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwampManState : CharacterState {
+
+    protected SwampManModel swampManModel;
+    protected Rigidbody2D rb2d;
+    protected Animator anim;
+
+    protected float timeBetweenAttacksCooldownSecurity = 0;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    public override void InitState()
+    {
+        swampManModel = GameManager.instance.swampManModel;
+        base.health = swampManModel.health;
+        base.initialHealth = swampManModel.health;
+
+        GetComponent<SwampManController>().enabled = true;
+    }
+
+    protected void changeAnim(Vector2 objectiveVector)
+    {
+        anim.SetFloat("moveX", objectiveVector.x);
+        anim.SetFloat("moveY", objectiveVector.y);
+    }
+}
