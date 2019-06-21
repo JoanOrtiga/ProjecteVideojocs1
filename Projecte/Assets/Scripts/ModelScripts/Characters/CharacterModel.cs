3efc4a8b243268397a00bc4d6769b70ea89148bb
LadyMarina <sharkk244@gmail.com>
2019-06-21 14:13:33 +0200

    dsa

--- Projecte/Assets/Scripts/ModelScripts/Characters/CharacterModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CharacterModel : ScriptableObject
-{
-    [Header("Character HP")]
-    public float health;
-}
--- Projecte/Assets/Scripts/ModelScripts/Characters/Enemies/SatanBoss2/SatanModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SatanModel", menuName = "Models/Characters/SatanModel")]
-public class SatanModel : EnemyModel {
-
-	
-}
