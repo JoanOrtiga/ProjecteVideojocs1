8a5e248c1e41410b0f082d0572430adb5830e399
LadyMarina <sharkk244@gmail.com>
2019-06-21 15:05:31 +0200

    asd

--- Projecte/Assets/Scripts/ModelScripts/Characters/Enemies/Skeleton/SkeletonModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EnemyModel", menuName = "Models/Characters/Enemy/Skeleton")]
-public class SkeletonModel : EnemyModel {
-
-}
