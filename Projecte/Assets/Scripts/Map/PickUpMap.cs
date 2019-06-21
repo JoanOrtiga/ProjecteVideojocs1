5307e3930fabd06db3d9fce7123cbb3b320baf04
LadyMarina <sharkk244@gmail.com>
2019-06-21 22:50:00 +0200

    MAP UPDATE

---
+++ Projecte/Assets/Scripts/Map/PickUpMap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpMap : MonoBehaviour {
+
+    public Image mapIcon;
+    public GameObject gameManager;
+	// Use this for initialization
+	void Start ()
+    {
+        mapIcon.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mapIcon.enabled = true;
+            gameManager.GetComponent<LoadMap>().pickUpMap = true;
+
+            Destroy(gameObject);
+        }
+    }
+
+}
