ceea6c3a3abb363db70c9ef5db0bed2b4cb8c939
LadyMarina <sharkk244@gmail.com>
2019-06-21 16:09:28 +0200

    tyumb

--- Projecte/Assets/Scripts/Objects/objectDestroyer.cs
+++ Projecte/Assets/Scripts/Objects/objectDestroyer.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tumbDestroyer : MonoBehaviour
+public class objectDestroyer : MonoBehaviour
 {
-   
+    public AudioClip audioDestroy;
+    public AudioSource destroyAudioSource;
+    private Animator anim;
 
     // Use this for initialization
     void Start()
     {
-      
+        destroyAudioSource.clip = audioDestroy;
+        anim = GetComponentInParent<Animator>();
     }
 
 
     public void Smash()
     {
-       
-        Destroy(gameObject.transform.parent.gameObject);
-       
+        anim.SetBool("smash", true);
+        destroyAudioSource.Play();
+        Destroy(gameObject.transform.parent.gameObject, .3f);
+        //Destroy(gameObject, .3f);
     }
 }
 
