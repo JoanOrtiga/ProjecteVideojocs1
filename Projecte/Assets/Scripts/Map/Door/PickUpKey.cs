41499f91f29efc4561558f4ec5ec154be1f0166a
LadyMarina <sharkk244@gmail.com>
2019-06-22 19:06:34 +0200

    key

---
+++ Projecte/Assets/Scripts/Map/Door/PickUpKey.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpKey : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource.clip = audioClip;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Play();
+            Destroy(gameObject, 0.8f);
+            //posar true al script de doorOpen;
+        }
+    }
+}
