6121dd0a4b7916bf8b6c01b572535c4a5d6bc191
JuliaBlasco <47471443+JuliaBlasco@users.noreply.github.com>
2019-06-23 19:35:25 +0200

    vdfvxx

---
+++ Projecte/Assets/Scripts/Objects/Doors/doorWay2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorWay2 : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayPickupRelicInvetory>().relic_cross)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
