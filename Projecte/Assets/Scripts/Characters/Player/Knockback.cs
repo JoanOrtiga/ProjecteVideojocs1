654c5a007714cd5f2555181d8c2b73636ef3d331
LadyMarina <sharkk244@gmail.com>
2019-06-23 12:57:32 +0200

    knockback

--- Projecte/Assets/Scripts/Characters/Player/Knockback.cs
+++ Projecte/Assets/Scripts/Characters/Player/Knockback.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class Knockback : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public float thrust;
+    public float knockTime;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            if (enemy != null)
+            {
+                enemy.isKinematic = false;
+                Vector2 difference =  enemy.transform.position - transform.position;
+                difference = difference.normalized * thrust;
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(enemy));
+               
+            }
+        }
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D enemy)
+    {
+        if (enemy != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            enemy.velocity = Vector2.zero;
+            enemy.isKinematic = true;
+        }
+    }
 }
