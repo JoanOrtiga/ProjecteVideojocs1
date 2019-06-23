b85ec86ecfe7d9bdc5dd9e5140d9a140cedbba48
JuliaBlasco <47471443+JuliaBlasco@users.noreply.github.com>
2019-06-23 15:34:04 +0200

    sfxa

--- Projecte/Assets/Scripts/Objects/Doors/closedDoorLvl2.cs
+++ Projecte/Assets/Scripts/Objects/Doors/closedDoorLvl2.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class closedDoorLvl2 : MonoBehaviour
 {
-    public GameObject openedDoor;
+    [SerializeField]
+    private GameObject openedDoor;
     private bool bossAlive = false;
 
     // Update is called once per frame
     void Update ()
     {
-        Debug.Log("Exists" + bossAlive);
         if (GameObject.Find("Boss1(Clone)") != null)
         {
             bossAlive = true;
@@ -20,7 +20,6 @@ public class closedDoorLvl2 : MonoBehaviour
         {
             if (GameObject.Find("Boss1(Clone)") == null)
             {
-                Debug.Log("Dead boss");
                 openedDoor.SetActive(true);
                 Destroy(gameObject);
             }
