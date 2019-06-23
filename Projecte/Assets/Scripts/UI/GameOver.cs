2e4307df48249749a7b601da8a0c98a03c43b843
JuliaBlasco <47471443+JuliaBlasco@users.noreply.github.com>
2019-06-23 15:00:24 +0200

    rv5ertg

---
+++ Projecte/Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject gameOver;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(GameManager.instance.getplayerHealth() <= 0)
+        {
+            gameOver.SetActive(true);
+            //Time.timeScale = 0f;
+        }
+
+        if (Input.GetButton("Restart"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
